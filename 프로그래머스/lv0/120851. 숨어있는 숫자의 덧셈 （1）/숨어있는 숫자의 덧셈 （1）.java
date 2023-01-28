c26282fe622dd65a1e34f4f740141d7a1f586bbc
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-01-28 11:54:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 4.13 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+       return Arrays.stream(Arrays.stream(my_string.replaceAll("[^0-9]", "").split(""))
+                                  .sorted().mapToInt(Integer::parseInt).toArray())
+                    .sum();
+    }
+}
