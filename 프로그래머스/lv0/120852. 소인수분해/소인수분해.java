22f7aeea4d2b79117d28b9d005f205ccee069542
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-01-28 11:54:34 +0900

    [level 0] Title: 소인수분해, Time: 3.04 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        
+        if(n < 2 && n > 10000){
+            return null;
+        }
+        
+        // LinkedHashSet
+        LinkedHashSet <Integer> set = new LinkedHashSet<>();
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                set.add(i);
+                n /= i;
+            }
+        }
+        
+        if (n != 1){
+            set.add(n);
+        }
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
