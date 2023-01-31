585a25937e66d1a61c8cd82bdd4c1cc174c25acd
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-01 08:58:05 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.41 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        // 문자열 배열 생성 (공백 기준으로 자름)
+        String[] str = s.split(" ");
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < str.length; i++) {
+            list.add(Integer.parseInt(str[i]));
+        }
+        
+        // Collections.min / max 사용
+        sb.append(Collections.min(list)).append(" ").append(Collections.max(list));
+        
+        return sb.toString();
+    }
+}
