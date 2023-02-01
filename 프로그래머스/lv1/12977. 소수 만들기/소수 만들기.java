aef5933c73ee33b0ca3841b73ae48ce4cc17c1a7
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-01 20:02:21 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,39 +1,36 @@
 class Solution {
-    int answer;
-    int sum;
-    public int solution(int[] nums) {
-        
-        boolean[] visited = new boolean[nums.length];
-
-       if(answer>0)
-        answer=0;
-       
-        combination(nums, visited,0,3);
-        
-        return answer;
-    }
-    public void combination(int[] nums, boolean[] visited, int depth,int out){
-        
-        if(out == 0){
-            sum = 0;
-             for(int i = 0; i < visited.length;i++){
-                if(visited[i]==true)
-                    sum+= nums[i];
-             }
-            for(int i = 2; i*i <=sum; i++){
-                if(sum % i == 0)
-                    return;
-                }
-                    answer++;
-                    return ;
-            }    
-        if(depth == visited.length){
-            return;
+    public boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
         }
         
-                visited[depth]=true;
-                combination(nums,visited,depth+1,out-1);
-                visited[depth]=false;
-                combination(nums,visited,depth+1,out);
+        // Math.sqrt() 쓰면 포함으로
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
             }
         }
+        return true;
+    }
+    
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+        
+        // 3중 포문
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int l = j + 1; l < nums.length; l++) {
+                    sum = nums[i] + nums[j] + nums[l];
+                    
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+
+    }
+}
