f56f7febdbb335a4c2be6ca97ed9518d825a0d21
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-02 18:44:57 +0900

    [level 2] Title: 올바른 괄호, Time: 16.92 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,45 @@
+import java.util.Stack;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
         
         // 개수 사용
-        int count = 0;
+//         int count = 0;
 
+//         for (char c : s.toCharArray()) {
+//             if (c == '(') {
+//                 count++;
+//             }
+            
+//             if (c == ')') {
+//                 count--;
+//             }
+            
+//             if (count < 0){
+//                 return false;
+//             }
+//         }
+
+        // return count == 0;
+        
+        // 문제대로 stack 사용
+        Stack<Character> stack = new Stack<>();
+        
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                count++;
-            }
-            
-            if (c == ')') {
-                count--;
-            }
-            
-            if (count < 0){
-                return false;
+                // ( 괄호면 넣기
+                stack.push(c);
+                
+            } else {     // ) 괄호면 스택이 비어있는지 확인 후 빼기
+                if(stack.isEmpty()){
+                    return false;
+                }
+                
+                stack.pop();
             }
         }
-
-        return count == 0;
+        
+        return stack.isEmpty();
     }
 }
