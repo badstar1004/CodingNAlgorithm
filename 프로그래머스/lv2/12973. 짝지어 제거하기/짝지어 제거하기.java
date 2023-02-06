a1838ef8feb9eb7d6d0ac1f3941879aca02531d2
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-06 10:49:07 +0900

    [level 2] Title: 짝지어 제거하기, Time: 46.97 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -1,10 +1,26 @@
 import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution{
     public int solution(String s){
         int answer = 0;
 
-        // stack 사용
+        /* ======= HashMap 사용 ======= */
+        // 카운트 값 체크 -> 틀린 답 / cdcd하면 바로 옆 문자와 비교를 해야함
+//         HashMap<Character, Integer> hashMap = new HashMap<>();
+        
+//         for(Character c : s.toCharArray()){
+//             hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+//         }
+
+//         for(Map.Entry<Character, Integer> val : hashMap.entrySet()){
+//             if(val.getValue() % 2 == 0){
+//                 answer = 1;
+//             }
+//         }
+        
+        /* ======== stack 사용 ======== */
         // 첫 문자 넣고 for문으로 같은거 찾음
         Stack<Character> stack = new Stack<>();
         
