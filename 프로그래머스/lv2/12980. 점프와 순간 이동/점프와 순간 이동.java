30f1e65221066fd960c6f8222d1626dd036662ed
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-06 09:44:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,13 +3,16 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        int cnt = 0;
-
-        // 홀수: ans += 1, n-- / 짝수: n /= 2;
+        
+        // 홀수인 경우에는 +1 짝수인 경우엔 *2 를 함
+        // n부터 거꾸로 내려가는걸로 생각하면
+        // 홀수: ans++, n-- / 짝수: n /= 2;
         // while문
         while(n > 0){
+            // 짝수일 경우
             if(n % 2 == 0){
                 n /= 2;
+                // 홀수 일 경우
             } else{
                 n--;
                 ans++;
