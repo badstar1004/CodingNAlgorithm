532fa86a2dd5eccd5f7d9191bfdd41fa4dcef675
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-08 00:45:20 +0900

    [level 2] Title: H-Index, Time: 0.32 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,20 +1,34 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        // 내림차순 정렬을 위해 Integer배열로 변경
+        Integer[] arr = new Integer[citations.length];
         
-        // 짝수 길이면 0
-        if(citations.length % 2 == 0){
-            return answer;
+        for(int i = 0; i < citations.length; i++){
+            arr[i] = citations[i];
         }
         
-        // 오름차순 정렬
-        Arrays.sort(citations);
+        // 내림차순 정렬
+        Arrays.sort(arr, Collections.reverseOrder());
         
-        while(answer + 1 <= citations[answer]){
-            answer++;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] >= i + 1){
+                answer++;
+            }
         }
+        
+        // 정렬
+        // Arrays.sort(citations);
+        
+        // for(int i = 0; i < citations.length; i++){
+        //     if(citations[i] >= citations.length - i){
+        //         answer = citations.length - i;
+        //         break;
+        //     }
+        // }
         
         return answer;
     }
