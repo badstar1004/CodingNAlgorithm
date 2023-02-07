f9d86e5cdec3d77e6996d223bad7e89390039d6e
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-07 20:36:16 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.44 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,34 +1,52 @@
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         
-        // 점수를 넣을 ArrayList
-        List<Integer> list = new ArrayList<>();
+        // 우선순위 큐 사용
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         
-        // for문
         for(int i = 0; i < score.length; i++){
-            // list.size() < k
-            if(list.size() < k){
-                list.add(score[i]);
-                
-            } else {
-                // list.get(0) < score[i]
-                if(list.get(0) < score[i]){
-                    // 0번째에 넣어줌
-                    list.set(0, score[i]);
-                }
+            // 넣을 때 자동으로 정렬이 됨
+            priorityQueue.add(score[i]);
+            
+            // priorityQueue.size() > k
+            if(priorityQueue.size() > k){
+                // priorityQueue 에서 빼냄
+                priorityQueue.poll();
             }
             
-            // 정렬
-            Collections.sort(list);
-            
-            // 작은 값 부터 넣어줌
-            answer[i] = list.get(0);
+            answer[i] = priorityQueue.peek();
         }
+        
+//         // List 사용
+//         // 점수를 넣을 ArrayList
+//         List<Integer> list = new ArrayList<>();
+        
+//         // for문
+//         for(int i = 0; i < score.length; i++){
+//             // list.size() < k
+//             if(list.size() < k){
+//                 list.add(score[i]);
+                
+//             } else {
+//                 // list.get(0) < score[i]
+//                 if(list.get(0) < score[i]){
+//                     // 0번째에 넣어줌
+//                     list.set(0, score[i]);
+//                 }
+//             }
+            
+//             // 정렬
+//             Collections.sort(list);
+            
+//             // 작은 값 부터 넣어줌
+//             answer[i] = list.get(0);
+//         }
         
         return answer;
     }
