be3805b2aa3ec8aac9a70fff14a65e398157f65c
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-08 01:03:54 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        
         // 내림차순 정렬을 위해 Integer배열로 변경
         Integer[] arr = new Integer[citations.length];
-        
         for(int i = 0; i < citations.length; i++){
             arr[i] = citations[i];
         }
@@ -15,7 +15,10 @@ class Solution {
         Arrays.sort(arr, Collections.reverseOrder());
         
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] >= i + 1){
+            // 피인용 수가 논문 수보다 더 작아지기 시작하는 직전의 수가 h이다 (유튜브 인용구)
+            // 다시 말하면 h = 논문 수가 피인용 수와 같거나 커질때의 수
+            // if문 = 같거나 커질 때 answer++ 해줌
+            if(arr[i] >= i + 1){ // 논문 수 => i + 1 -> 배열 0부터 시작이라 1부터
                 answer++;
             }
         }
