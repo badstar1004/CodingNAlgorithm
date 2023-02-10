5a8e0c42a661360875080cfd35c9cd45fb2fa295
기믄정 <32833969+badstar1004@users.noreply.github.com>
2023-02-10 23:48:54 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < n; i++){
+            sb.append(i % 2 == 0 ? "수" : "박");
+        }
+        
+        return sb.toString();
+    }
+}
