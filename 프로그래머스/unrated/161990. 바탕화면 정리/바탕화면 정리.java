f36cacdf19ce83f1e3b408664410f2e77ed296dd
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-07-27 21:45:44 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.11 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -8,15 +8,18 @@ class Solution {
         */
         
         int row = wallpaper.length;
-        int col = wallpaper[0].length();
+        int cor = wallpaper[0].length();
         
         // 직사각형 초기화
-        int minX = 0, minY = 0, maxX = col, maxY = row;
+        int minX = 0;     
+        int minY = 0; 
+        int maxX = cor;   
+        int maxY = row; 
         
-        // 이중 for문
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (wallpaper[i].charAt(j) == '#'){
+        // 2중 for문
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < cor; j++) {
+                if(wallpaper[i].charAt(j) == '#') {
                     minX = Math.max(minX, j);
                     minY = Math.max(minY, i);
                     maxX = Math.min(maxX, j);
@@ -26,5 +29,6 @@ class Solution {
         }
         
         return new int[]{maxY, maxX, minY + 1, minX + 1};
+        
     }
 }
