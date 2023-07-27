d6b11dde709dd0b6bbafe0d57fbf30d3d5ce711d
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-07-27 22:50:40 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.89 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,25 +1,76 @@
+import java.util.*;
+
 class Solution {
     public String solution(String new_id) {
         
-        // 정규식
-        String answer = new_id.toLowerCase()    // 1단계
-            .replaceAll("[^-_.a-z0-9]", "")     // 2단계
-            .replaceAll("[.]{2,}", ".")         // 3단계
-            .replaceAll("^[.]|[.]$", "");       // 4단계
-        
-        if(answer.equals("")) {                 // 5단계
-            answer += "a";    
+        StringBuilder sb = new StringBuilder();
+        // StringBuilder
+        // 1단계 & 2단계
+        for (char c : new_id.toCharArray()) {
+            if (Character.isAlphabetic(c) || Character.isDigit(c)
+                        || c == '-' || c == '_' || c == '.') {
+                sb.append(Character.toLowerCase(c));
+            }
         }
         
-        if(answer.length() >= 16) {             // 6단계
-            answer = answer.substring(0, 15);
-            answer = answer.replaceAll("[.]$", "");
+        // 3단계
+        for (int i = 1; i < sb.length(); i++) {
+            if (sb.charAt(i) == '.' && sb.charAt(i - 1) == '.') {
+                sb.deleteCharAt(i);
+                i--;
+            }
         }
         
-        while(answer.length() <= 2) {           // 7단계
-            answer += answer.charAt(answer.length() - 1);
+        // 4단계
+        if (sb.length() > 0 && sb.charAt(0) == '.') {   // 처음
+            sb.deleteCharAt(0);
+        }
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') { // 끝
+            sb.deleteCharAt(sb.length() - 1);
+        } 
+        
+        // 5단계
+        if(sb.length() == 0) {
+            sb.append('a');
         }
         
-        return answer;
+        // 6단계
+        if(sb.length() >= 16) {
+            sb.delete(15, sb.length());
+            
+            if(sb.charAt(sb.length() - 1) == '.') {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+        
+        // 7단계
+        while(sb.length() < 3) {
+            sb.append(sb.charAt(sb.length() - 1));
+        }
+        
+        
+        return sb.toString();
+        
+        
+//         // 정규식
+//         String answer = new_id.toLowerCase()    // 1단계
+//             .replaceAll("[^-_.a-z0-9]", "")     // 2단계
+//             .replaceAll("[.]{2,}", ".")         // 3단계
+//             .replaceAll("^[.]|[.]$", "");       // 4단계
+        
+//         if(answer.equals("")) {                 // 5단계
+//             answer += "a";    
+//         }
+        
+//         if(answer.length() >= 16) {             // 6단계
+//             answer = answer.substring(0, 15);
+//             answer = answer.replaceAll("[.]$", "");
+//         }
+        
+//         while(answer.length() <= 2) {           // 7단계
+//             answer += answer.charAt(answer.length() - 1);
+//         }
+        
+        // return answer;
     }
 }
