160714ff0cb2ebb377ca8c9b59a5c51c632ae109
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-03 21:14:36 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,7 +3,27 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        return IntStream.range(0, 10)
-            .filter(i -> Arrays.stream(numbers).noneMatch(num -> i == num)).sum();
+        int answer = 0;
+        boolean[] exist = new boolean[10];  // 초기값은 false
+
+        // numbers에 있는 숫자들 true로 수정
+        for (int num : numbers) {
+            if (num < 10) {
+                exist[num] = true;
+            }
+        }
+        
+        // false 인 요소들만 더함
+        for(int i = 0; i < 10; i++) {
+            if(!exist[i]) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+        
+        // Stream 사용
+        // return IntStream.range(0, 10)
+        //     .filter(i -> Arrays.stream(numbers).noneMatch(num -> i == num)).sum();
     }
 }
