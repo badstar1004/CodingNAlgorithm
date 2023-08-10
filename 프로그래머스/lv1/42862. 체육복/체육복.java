2badf7aa66ec9c80e1ca465163f469ff0fdca2e9
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-10 21:35:54 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,39 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 학생 수
-        int answer = n;
+        // 배열 카운트 세기
+        int[] student = new int[n];
         
-        // 정렬
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-
-        // 옷이 있는 사람과 없는 사람 배열
-        int[] people = new int[n];
-
-        // lost 인 배열값 - 1 (= 배열 0번째부터 주소값, 빼줌 (기본값 0))
-        for (int l : lost){
-            people[l - 1]--;
+        // 도난당한 학생들 인덱스 카운트 --
+        for(int l : lost) {
+            student[l - 1]--;
         }
-
-        // reserve 인 배열값 - 1 (= 배열 0번째부터 주소값, 더해줌 (기본값 0))
-        for (int r : reserve){
-            people[r - 1]++;
+        
+        // 여벌이 있는 학생들 인덱스 카운트++
+        for(int r : reserve) {
+            student[r - 1]++;
         }
-
-        for (int i = 0; i < people.length; i++) {
-            // 옷이 없는 사람이라면
-            if(people[i] == -1) {
-                // 앞에 옷이 있는 사람
-                if(i - 1 >= 0 && people[i - 1] == 1) {
-                    people[i]++;
-                    people[i - 1]--;
-                    // 뒤에 옷이 있는 사람
-                } else if(i + 1 < people.length && people[i + 1] == 1) {
-                    people[i]++;
-                    people[i + 1]--;
-                } else
-                    answer--;
+        
+        for(int i = 0; i < student.length; i++) {
+            if(student[i] == -1) {
+                if(i > 0 && student[i - 1] == 1) {
+                    student[i - 1]--;
+                    student[i]++;
+                    
+                } else if(i < student.length - 1 && student[i + 1] == 1) {
+                    student[i + 1]--;
+                    student[i]++;
+                }
+            }
+        }
+        
+        int answer = 0;
+        for(int s : student) {
+            if(s >= 0) {
+                answer++;
             }
         }
         
