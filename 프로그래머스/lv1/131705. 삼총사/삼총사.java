7ef29afd6400947ecd3dafa7340ecee822105084
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-10 21:07:11 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,10 +1,8 @@
-import java.util.HashMap;
-
 class Solution {
     public int solution(int[] number) {
         int answer = 0;
         
-        // 3중 for문 (수정이 필요)
+        // 3중 for문 (다른 방법이 있을까?)
         for(int i = 0; i < number.length - 2; i++){
             for(int j = i + 1; j < number.length - 1; j++){
                 for(int k = j + 1; k < number.length; k++){
