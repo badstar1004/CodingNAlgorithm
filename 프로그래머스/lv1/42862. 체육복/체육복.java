e3e834156b43aa5b375a055f86a632cec6a7d1f6
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-10 21:52:49 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // 배열 카운트 세기
+        // 배열 카운트
         int[] student = new int[n];
         
         // 도난당한 학생들 인덱스 카운트 --
@@ -15,14 +15,19 @@ class Solution {
             student[r - 1]++;
         }
         
+        // for문
         for(int i = 0; i < student.length; i++) {
+            // 체육복을 도난 당한 학생이라면
             if(student[i] == -1) {
+                
+                // 앞번호 학생이 여벌의 체육복을 가지고 있는 경우 (첫번째 학생 제외)
                 if(i > 0 && student[i - 1] == 1) {
                     student[i - 1]--;
                     student[i]++;
                     
+                // 뒷번호 학생이 여벌의 체육복을 가지고 있는지 확인 (마지막 학생 제외)
                 } else if(i < student.length - 1 && student[i + 1] == 1) {
-                    student[i + 1]--;
+                    student[i + 1]--;   
                     student[i]++;
                 }
             }
