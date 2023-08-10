8297bf1c4354293266611d003637231981301139
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-10 21:54:48 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -3,9 +3,9 @@ class Solution {
         int answer = 0;
         
         // 3중 for문 (다른 방법이 있을까?)
-        for(int i = 0; i < number.length - 2; i++){
-            for(int j = i + 1; j < number.length - 1; j++){
-                for(int k = j + 1; k < number.length; k++){
+        for(int i = 0; i < number.length - 2; i++) {
+            for(int j = i + 1; j < number.length - 1; j++) {
+                for(int k = j + 1; k < number.length; k++) {
                     if(number[i] + number[j] + number[k] == 0){
                         answer++;
                     }
