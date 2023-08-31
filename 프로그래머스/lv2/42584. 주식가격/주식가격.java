1f3df67633fe001a851ed111f6ce99defdff5290
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-08-31 20:40:07 +0900

    [level 2] Title: 주식가격, Time: 13.67 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -8,8 +8,8 @@ class Solution {
         */
         
         answer = new int[prices.length];
-        for(int i = 0; i < prices.length; i++){
-            for(int j = i + 1; j < prices.length; j++){
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i + 1; j < prices.length; j++) {
                 answer[i]++;
                 if (prices[i] > prices[j]) {
                     break;
