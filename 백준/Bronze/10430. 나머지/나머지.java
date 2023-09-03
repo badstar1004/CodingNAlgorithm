23c9ade739b08006f2c69fbfe545e6e462897715
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-09-03 23:22:34 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        
+        StringTokenizer st = new StringTokenizer(input, " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}
