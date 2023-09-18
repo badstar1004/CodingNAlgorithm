ff41012560ef3133d6906aeccc13ea853db93a77
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-09-18 23:33:09 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.08 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,48 @@
+class Solution {
+    
+    private static boolean isPrime(long number) {
+        if(number <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private static String convertBinary(int n, int k) {
+        StringBuilder str = new StringBuilder();
+ 
+        while (n > 0) {
+            str.append(n % k);
+            n /= k;
+        }
+ 
+        // str = str.reverse();
+        return str.reverse().toString();
+    }
+    
+    public int solution(int n, int k) {
+        int answer = -1;
+        
+        // 변환
+        String convert = convertBinary(n, k);
+        
+        // split
+        String[] split = convert.split("0");
+        for (String x : split) {
+            if (x.equals("")) {
+                continue;
+            }
+            
+            if (isPrime(Long.parseLong(x))) {
+                answer++;
+            }
+        }
+        
+        return answer + 1;
+    }
+}
