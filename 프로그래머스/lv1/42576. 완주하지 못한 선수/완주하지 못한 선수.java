5d4a8b4586132e8ade66c00a1a8c6b60932dac47
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-09-21 20:31:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.32 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,32 @@
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.*;
+import java.util.Map.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
 
         String answer = "";
-
+        
+        // HashMap
         HashMap<String, Integer> map = new HashMap<>();
-
+        
+        // 선수 명단
         for (String part : participant) {
             map.put(part, map.getOrDefault(part, 0) + 1);
         }
 
+        // 완주자
         for (String comp : completion) {
             map.put(comp, map.get(comp) - 1);
         }
 
-        for (Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > 0) {
-                answer = entry.getKey();
+        // 결과
+        for(Map.Entry<String, Integer> result : map.entrySet()) {
+            if(result.getValue() > 0) {
+                answer = result.getKey();
                 break;
             }
         }
+        
         return answer;
     }
 }
