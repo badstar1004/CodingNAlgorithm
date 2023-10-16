1de1b69897cd9e4c135ad1349589593598b2c34c
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-10-17 00:05:11 +0900

    [level 2] Title: 양궁대회, Time: 0.75 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -10,23 +10,8 @@ public class Solution {
     private void dfs(int arrow, int[] apeach, int[] lion, int idx) {
         // return 값
         if (idx == 11) {
-            int apeachSum = 0, lionSum = 0;
-            for (int i = 0; i < 11; i++) {
-                // apeach == lion
-                if (apeach[i] == 0 && lion[i] == 0) {
-                    continue;
-                }
-                
-                // apeach >= lion
-                if (apeach[i] >= lion[i]) {
-                    apeachSum += (10 - i);
-                    
-                } else {
-                    lionSum += (10 - i);
-                }
-            }
-            
-            int diff = lionSum - apeachSum;
+            // 비교 계산
+            int diff = calDiff(apeach, lion);
             if (diff > 0 && (diff > maxDiff || (diff == maxDiff && isBetter(lion)))) {
                 maxDiff = diff;
                 answer = lion.clone();
@@ -34,21 +19,44 @@ public class Solution {
             return;
         }
 
+        // 라이언이 해당 점수에 화살을 쏘지 않는 경우
+        dfs(arrow, apeach, lion, idx + 1);
+        
+        
         if (arrow > apeach[idx]) {
             lion[idx] = apeach[idx] + 1;
-            
             dfs(arrow - lion[idx], apeach, lion, idx + 1);
             lion[idx] = 0;
         }
         
-        dfs(arrow, apeach, lion, idx + 1);
         if (idx == 10 && arrow > 0) {
             lion[idx] += arrow;
             dfs(0, apeach, lion, idx + 1);
             lion[idx] = 0;
         }
     }
+    
+    // 비교 계산
+    private int calDiff(int[] apeach, int[] lion) {
+        int apeachSum = 0, lionSum = 0;
+        for (int i = 0; i < 11; i++) {
+            // apeach == lion
+            if (apeach[i] == 0 && lion[i] == 0) {
+                continue;
+            }
 
+            // apeach >= lion
+            if (apeach[i] >= lion[i]) {
+                apeachSum += (10 - i);
+
+            } else {
+                lionSum += (10 - i);
+            }
+        }
+        return lionSum - apeachSum;
+    }
+    
+    
     private boolean isBetter(int[] lion) {
         for (int i = 10; i >= 0; i--) {
             if (answer[i] < lion[i]) {
