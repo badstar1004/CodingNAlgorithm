9992734dd50cc59ef330e3028f0024e22061c492
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-11-20 20:53:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.71 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,45 +1,46 @@
 import java.util.*;
 
+class Truck {
+    int weight;
+    int distanceLeft;
+
+    public Truck(int weight, int distanceLeft) {
+        this.weight = weight;
+        this.distanceLeft = distanceLeft;
+    }
+}
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
         
-        Queue<Integer> bridge = new LinkedList<>();
-        int totalWeight = 0;
+         Queue<Truck> bridge = new LinkedList<>();
         int time = 0;
-        
-        for (int truck : truck_weights) {
-            while (true) {
-                // 다리가 비어있는 경우
-                if (bridge.isEmpty()) {
-                    bridge.add(truck);
-                    totalWeight += truck;
-                    time++;
-                    break;
-                }
-                // 다리가 가득 찬 경우
-                else if (bridge.size() == bridge_length) {
-                    totalWeight -= bridge.poll();
-                }
-                // 다리에 여유가 있는 경우
-                else {
-                    // 다음 트럭이 올라갈 수 있는 경우
-                    if (totalWeight + truck <= weight) {
-                        bridge.add(truck);
-                        totalWeight += truck;
-                        time++;
-                        break;
-                    } 
-                    // 다음 트럭이 올라갈 수 없는 경우
-                    else {
-                        bridge.add(0); // 무게 0인 가상의 트럭을 추가
-                        time++;
-                    }
-                }
+        int totalWeightOnBridge = 0;
+        int index = 0;
+
+        while (index < truck_weights.length || !bridge.isEmpty()) {
+            time++;
+
+            // 다리 위의 트럭이 다리를 건너면 다리에서 제거
+            if (!bridge.isEmpty() && bridge.peek().distanceLeft <= 0) {
+                totalWeightOnBridge -= bridge.poll().weight;
+            }
+
+            // 새 트럭이 다리에 진입할 수 있는지 확인
+            if (index < truck_weights.length && totalWeightOnBridge + truck_weights[index] <= weight) {
+                Truck truck = new Truck(truck_weights[index], bridge_length);
+                bridge.add(truck);
+                totalWeightOnBridge += truck.weight;
+                index++;
+            }
+
+            // 다리 위의 모든 트럭의 남은 거리 업데이트
+            for (Truck t : bridge) {
+                t.distanceLeft--;
             }
         }
-        
-        // 마지막 트럭이 다리를 건너는 시간 추가
-        return time + bridge_length;
+
+        return time;
     }
 }
