eaedb952ea7870736e2f65efe46d5f0cf4dee8ce
김은정 <32833969+badstar1004@users.noreply.github.com>
2023-11-27 20:31:18 +0900

    [level 2] Title: 소수 찾기, Time: 11.76 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    
+    // 판별된 소수들
+    private Set<Integer> primes = new HashSet<>();
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        
+        /*
+            변수
+                HashSet
+            매서드
+                재귀
+                소수판별
+        */
+        
+        recursion("", numbers);
+        return primes.size();
+    }
+    
+    private void recursion(String prefix, String numbers) {
+        if(prefix != "") {
+            int num = Integer.parseInt(prefix);
+            
+            if(isPrime(num)) {
+                primes.add(num);
+            }
+        }
+        
+        for(int i = 0; i < numbers.length(); i++) {
+            recursion(prefix + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
+        }
+    }
+    
+    // 소수 판별
+    private boolean isPrime(int num) {
+        if(num <= 1) {
+            return false;
+        }
+        
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
