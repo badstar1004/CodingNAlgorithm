b011fb5c86129ea01132eddbe3d1252ca09faddb
이너프(Enough) <32833969+badstar1004@users.noreply.github.com>
2024-07-15 21:00:16 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.76 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,33 +2,54 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int[] stk = {};
+        /* Stack */
         
-        Stack<Integer> stack = new Stack<>();
-        int i = 0;
+//         Stack<Integer> stack = new Stack<>();
+//         int i = 0;
         
-        while(i < arr.length) {
-            if(stack.isEmpty()) {
-                // stack.isEmpty() ? arr[i] 추가 i++
-                stack.push(arr[i]);
-                i++;
+//         while(i < arr.length) {
+//             if(stack.isEmpty()) {
+//                 // stack.isEmpty() ? arr[i] 추가 i++
+//                 stack.push(arr[i]);
+//                 i++;
                 
-            } else if(stack.peek() < arr[i]) {
-                // 마지막 원소 < arr[i] ? arr[i] 추가 i++
-                stack.push(arr[i]);
-                i++;
+//             } else if(stack.peek() < arr[i]) {
+//                 // 마지막 원소 < arr[i] ? arr[i] 추가 i++
+//                 stack.push(arr[i]);
+//                 i++;
                 
-            } else {
-                // 마지막 원소 >= arr[i] ? 마지막 원소 제거
-                stack.pop();
-            }
-        }
+//             } else {
+//                 // 마지막 원소 >= arr[i] ? 마지막 원소 제거
+//                 stack.pop();
+//             }
+//         }
         
-        stk = new int[stack.size()];
-        for(int j = stack.size() - 1; j >= 0; j--) {
-            stk[j] = stack.pop();
-        }
+//         stk = new int[stack.size()];
+//         for(int j = stack.size() - 1; j >= 0; j--) {
+//             stk[j] = stack.pop();
+//         }
         
-        return stk;
+         /* Array */
+         int[] stk = new int[arr.length];
+         int stkSize = 0;
+         int i = 0;
+        
+         while(i < arr.length) {
+             if(stkSize == 0) {
+                 stk[stkSize++] = arr[i++];
+                 
+             } else if(stk[stkSize - 1] < arr[i]) {
+                 stk[stkSize++] = arr[i++];
+                 
+             } else {
+                 stkSize--;
+             }
+         }
+        
+         int[] result = new int[stkSize];
+         System.arraycopy(stk, 0, result, 0, stkSize);
+        
+        
+         return result;
     }
 }
