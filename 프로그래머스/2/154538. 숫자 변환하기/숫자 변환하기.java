653fc0ad6da1a6d947a0398cf7b222f47cb80879
이너프(Enough) <32833969+badstar1004@users.noreply.github.com>
2024-10-14 19:25:42 +0900

    [level 2] Title: 숫자 변환하기, Time: 101.38 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int x, int y, int n) {
+        Queue<int[]> queue = new LinkedList<>();
+        
+        boolean[] visited = new boolean[y + 1];
+        
+        // 시작점
+        queue.add(new int[] {x, 0});
+        visited[x] = true;
+        
+        while(!queue.isEmpty()) {
+            int[] arrCur = queue.poll();
+            int curValue = arrCur[0];
+            int curStep = arrCur[1];
+            
+            // y값에 도달한 경우
+            if(curValue == y) {
+                return curStep;
+            }
+            
+            // 세 가지 연산 규칙 적용
+            int[] arrNextValue = {curValue + n, curValue * 2, curValue * 3};
+            
+            for(int nextValue : arrNextValue) {
+                if(nextValue <= y && !visited[nextValue]) {
+                    visited[nextValue] = true;
+                queue.add(new int[] {nextValue, curStep + 1});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
