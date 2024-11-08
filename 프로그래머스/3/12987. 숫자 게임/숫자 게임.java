93d7b4b187f0804eaaf76c3d3d18b0f93e59db05
이너프(Enough) <32833969+badstar1004@users.noreply.github.com>
2024-11-08 13:05:07 +0900

    [level 3] Title: 숫자 게임, Time: 104.66 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int indexA = 0;
+        int indexB = 0;
+        
+        while(indexB < B.length) {
+            if(B[indexB] > A[indexA]) {
+                answer++;
+                indexA++;
+            }
+            
+            indexB++;
+        }
+                
+        return answer;
+    }
+}
